8a12ff0609f7a114bcf2f4da5b950d2b95219a40
Marcus Carlberg <carlber.marcus@gmail.com>
2013-04-18 13:57:41 +0200

    Table layout done

--- Snake/src/client/GUI.java
+++ Snake/src/client/GUI.java
@@ -6,9 +6,9 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
-import javax.swing.JList;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTable;
 
@@ -24,6 +24,7 @@ public class GUI extends JFrame{
 	private JTextField textField_3;
 	JButton btnConnect;
 	private JTable table;
+	private JTextField textField_4;
 	
 	
 	public GUI() {
@@ -56,8 +57,17 @@ public class GUI extends JFrame{
 		textField_3.setColumns(10);
 		
 		JButton btnNewGame = new JButton("New Game");
-		btnNewGame.setBounds(60, 208, 117, 29);
+		btnNewGame.setBounds(67, 276, 117, 29);
 		settingsPanel.add(btnNewGame);
+		
+		JLabel lblPassword = new JLabel("Password*:");
+		lblPassword.setBounds(50, 208, 97, 16);
+		settingsPanel.add(lblPassword);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(50, 236, 134, 28);
+		settingsPanel.add(textField_4);
+		textField_4.setColumns(10);
 		
 		JPanel serverPanel = new JPanel();
 		tabbedPane.addTab("Servers", null, serverPanel, null);
@@ -96,18 +106,38 @@ public class GUI extends JFrame{
 		String[] columnNames = {"Server Name",
                 "Server IP",
                 "Server Port",
-                "# of Current Players",
+                "# of Players",
                 "Private"};
-		Object [][] data = new Object[5][10];
+		//TODO This data is temporary for display purposes, will be changed later.
+		Object[][] data = {
+			    {"Kathy", "Smith",
+			     30000, new Integer(5), new Boolean(false)},
+			    {"John", "Doe",
+			     10, new Integer(3), new Boolean(true)},
+			    {"Sue", "Black",
+			     2000, new Integer(2), new Boolean(false)},
+			    {"Jane", "White",
+			     4311, new Integer(20), new Boolean(true)},
+			    {"Joe", "Brown",
+			     13431, new Integer(10), new Boolean(false)}
+			};
+		
+
+		
 		table = new JTable(data, columnNames);
-		table.setBounds(22, 41, 328, 444);
-		serverPanel.add(table);
+		table.setBounds(22, 41, 375, 444);
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(22, 41, 402, 444);
+		table.setFillsViewportHeight(true);
+		serverPanel.add(scrollPane);
 	}
 	//CS was here
 	
 	public void setConnectListener(ActionListener list){
 		btnConnect.addActionListener(list);
 	} 
+	
+	
 	
 	public static void main(String[] args){
 		GUI gui = new GUI();
