2dfbda669705e36627c333381b1b60c49080e8d8
tyyk <oscar.e.andersson@gmail.com>
2013-04-27 16:09:28 +0200

    Added a few (non-Junit) tests for messagehandler using fake server/client

--- Snake/src/Tests/MessageHandlerTests.java
+++ Snake/src/Tests/MessageHandlerTests.java
@@ -15,36 +15,35 @@ public class MessageHandlerTests {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		ServerSocket serverSocket;
 		try {
-			serverSocket = new ServerSocket();
+			serverSocket = new ServerSocket(30000);
 			Socket socket = serverSocket.accept();
 			MessageHandler mh = new MessageHandler(socket);
 			String message = "";
 			Position pos;
 
-			while (true) {
-				
-				int code = mh.recieveCode();
-				
-				switch (code) {
-				
-				case Protocol.COM_SEND_POSITION:	pos = mh.recievePosition();
-													System.out
-															.println(pos);
-													mh.sendCode(Protocol.ANS_ACK);
-													break;
-				default: 							System.out.println("default");
-													break;	
-				}
+
+
+			int code = mh.recieveCode();
+
+			switch (code) {
+
+			case Protocol.COM_SEND_POSITION:	pos = mh.recievePosition();
+			System.out
+			.println(pos);
+			mh.sendCode(Protocol.ANS_ACK);
+			break;
+			default: 							System.out.println("default");
+			break;	
 			}
-			
-			
-			
-			
-			
-			
+
+
+
+
+
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
