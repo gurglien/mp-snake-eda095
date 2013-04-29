ddce4f621ead42334cd350507c7b2019978a985f
Christina Schmidt <dt08cs6@hacke-12.student.lth.se>
2013-04-29 11:12:26 +0200

    Made DServer an interruptable thread

--- Snake/src/autodetect/DServer.java
+++ Snake/src/autodetect/DServer.java
@@ -3,18 +3,22 @@ package autodetect;
 import java.net.*;
 import java.io.*;
 
-public class DServer {
+public class DServer extends Thread {
 	public static final String MESSAGE = "Game server (group 22) running on port ";
 	public static final int DSERVER_PORT = 4567;
 
-    public static void main(String args[]) {
-    	int gameServerPort = 1234; //TODO ska vara samma som spelets...
-
-	   	try {
-		    MulticastSocket ms = new MulticastSocket(DSERVER_PORT);
-		    InetAddress ia = InetAddress.getByName("experiment.mcast.net");
-		    ms.joinGroup(ia);
-		    while(true) {
+	private int gameServerPort;
+	
+	public DServer(int port) {
+		this.gameServerPort = port;
+	}
+	
+	public void run() {
+		try {
+			MulticastSocket ms = new MulticastSocket(DSERVER_PORT);
+			InetAddress ia = InetAddress.getByName("experiment.mcast.net");
+			ms.joinGroup(ia);
+			while(!interrupted()) {
 				byte[] buf = new byte[65536];
 				DatagramPacket dp = new DatagramPacket(buf,buf.length);
 				ms.receive(dp);
@@ -26,9 +30,14 @@ public class DServer {
 				buf = (MESSAGE + gameServerPort).getBytes();
 				dp = new DatagramPacket(buf, buf.length, cAddr, cPort);
 				socket.send(dp);
-		    }
+			}
 		} catch(IOException e) {
-		    System.out.println("Exception:"+e);
+			System.out.println("Exception:"+e);
 		}
+	}
+
+	public static void main(String args[]) {
+		DServer d = new DServer(1234);
+		d.start();
     }
 }
