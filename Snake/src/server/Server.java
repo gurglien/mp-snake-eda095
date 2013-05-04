86a3d05296c7a8a15bc7bdae9df8b3cbf082717c
tyyk <oscar.e.andersson@gmail.com>
2013-05-04 10:50:55 +0200

    added blajj to Server

--- Snake/src/server/Server.java
+++ Snake/src/server/Server.java
@@ -1,5 +1,44 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import client.Position;
+
+import common.MessageHandler;
+
 public class Server {
 
+	public static void main(String[] args) {
+
+
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(30000);
+			String message = "";
+			Position pos;
+			int code;
+			
+			
+//			Socket socket = serverSocket.accept();
+//			MessageHandler mh = new MessageHandler(socket);
+			
+			//vänta på inkommande connections, skapa trådar
+			
+			
+			
+			
+	
+			
+			
+			
+			
+			
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
